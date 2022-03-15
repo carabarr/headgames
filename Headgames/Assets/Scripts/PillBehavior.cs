008beceb0d4b6d7b138bc6f6627be5bc24c35422
Claudia <cpatricia.ab@gmail.com>
2022-03-15 17:02:09 -0400

    Added a playable prototype that uses arrow keys

---
+++ Headgames/Assets/Scripts/PillBehavior.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillBehavior : MonoBehaviour
+{
+
+    private float speed = 2f;
+
+    //Moves this GameObject 2 units a second in the forward direction
+    void Update()
+    {
+        transform.Translate(Vector3.back * Time.deltaTime * speed);
+    }
+
+    //Upon collision with another GameObject, this GameObject will reverse direction
+    private void OnTriggerEnter(Collider other)
+    {
+        //speed = speed * -1;
+        Destroy(gameObject);
+        Debug.Log("Ball Collided with StarPiece");
+    }
+
+
+}
