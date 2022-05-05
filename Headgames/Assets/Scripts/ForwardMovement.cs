ee41dd0691a4d72f6c1f9c57b3ba7e4981e32782
cpatricia-ab <cpatricia.ab@gmail.com>
2022-05-04 23:18:20 -0400

    fixed compilation issues
    recommented stuff that was actually crucial to the game lmao

--- Headgames/Assets/Scripts/ForwardMovement.cs
+++ Headgames/Assets/Scripts/ForwardMovement.cs
@@ -6,7 +6,9 @@ public class ForwardMovement : MonoBehaviour
 {
 
     public float speed;
+	public float acceleration;
     private float ogSpeed;
+	private float maxSpeed = 13f;
 	private float addScore;
 
     private Vector3 dir = Vector3.forward;
@@ -20,10 +22,32 @@ public class ForwardMovement : MonoBehaviour
 		
     }
 
+    public void SetSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+	}
+
+    public void restoreSpeed()
+    {
+        speed = ogSpeed;
+		FuelBar.fuelDepletion = 1;
+
+    }
 
     // Update is called once per frame
     void Update()
     {
+			// speed += acceleration;
+
+			// if (speed == maxSpeed/2){
+			// 	Debug.Log("WE ARE ON ON DEPLETION LEVEL 2");
+			// 	FuelBar.fuelDepletion = 2;
+			// }
+ 
+			// if (speed > (maxSpeed - 2)){
+			// 	speed = maxSpeed;
+			// 	FuelBar.fuelDepletion = 3;
+			// }
 
         // moves everything forward
         transform.Translate(dir * speed * Time.deltaTime);
